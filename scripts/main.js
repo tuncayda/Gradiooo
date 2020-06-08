6dd31138a2e87e544a1b51715834233b8b11d100
Tuncay Dagdelen <tuncaydagdelen89@users.noreply.github.com>
2020-06-08 23:49:31 +0200

    configured json package scripts

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,4 @@
 // const t = require('./color');
 // t();
 
-require('dotenv').config({path: __dirname + '../.env'});
-console.log(process.env.TEST);
-console.log(require('dotenv').config())
+console.log('heeej');
