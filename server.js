ff26f84045695dec665cdd5b58b1be24d5b8bd7b
Tuncay Dagdelen <tuncaydagdelen89@users.noreply.github.com>
2020-06-12 10:25:35 +0200

    Created the color schema

--- server.js
+++ server.js
@@ -12,6 +12,43 @@ mongoose.connect(DB_URI, {
     useFindAndModify: false
 }).then(() => console.log('Database connection successfull'));
 
+// Define the color schema
+const colorSchema = new mongoose.Schema({
+    author: {
+        type: String,
+        required: false,
+        maxlength: [30, 'Maximum 30 characters']
+    },
+    title: {
+        type: String,
+        required: [true, 'Name the color'],
+        unique: [true, 'The color title is already taken'],
+        minlength: 1,
+        maxlength: [30, 'Maximum 30 charachters']
+    },
+    colors: {
+        type: [String],
+        required: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    rating: {
+        type: Number,
+        min: 0,
+        default: 0
+    },
+    downloads: {
+        type: Number,
+        min: 0,
+        default: 0
+    }
+});
+
+// Create a model from the color schema
+const Color = mongoose.model('Color', colorSchema);
+
 // Start server
 const port = process.env.PORT || 8080;
 app.listen(port, function() {
