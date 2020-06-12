2f1d45141c9823fc03574b29f2a3422dbe6e308b
Tuncay Dagdelen <tuncaydagdelen89@users.noreply.github.com>
2020-06-12 10:19:11 +0200

    created conncetion to mongodb atlas

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const express = require('express');
-const mongoose = require('mongoose');
 const morgan = require('morgan');
 const app = express();
 const colorRouter = require('./routes/colorRoutes');
@@ -21,66 +20,5 @@ app.get('/', function(req, res) {
 // Mounting routers to endpoints
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/colors', colorRouter);
-
-// Connect to db
-// const DB_URI = process.env.DATABASE_URI;
-// mongoose.connect(DB_URI, {
-//     useUnifiedTopology:true,
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false
-// }).then(con => {
-//     console.log('DB connection successfull');
-// });
-
-// // The color schema
-// const colorSchema = new mongoose.Schema({
-//     author: {
-//         type: String,
-//         required: false,
-//         maxlength: [30, 'Maximum 30 characters']
-//     },
-//     title: {
-//         type: String,
-//         required: [true, 'Name your color'],
-//         unique: true,
-//         minlength: 1,
-//         maxlength: [30, 'Maximum 30 charachters']
-//     },
-//     colors: {
-//         type: [String],
-//         required: true
-//     },
-//     created: {
-//         type: Date,
-//         default: Date.now
-//     },
-//     rating: {
-//         type: Number,
-//         min: 0,
-//         default: 0
-//     },
-//     downloads: {
-//         type: Number,
-//         min: 0,
-//         default: 0
-//     }
-// });
-
-// // Create color model of the color schema
-// const Color = mongoose.model('Color', colorSchema);
-
-// // Test document
-// const testColor = new Color({
-//     author: 'Tuncay Dagdelen',
-//     title: 'Dreamlike',
-//     colors: ['fad0c4', 'ffd1ff']
-// });
-
-// testColor.save().then(doc => {
-//    console.log(doc);
-// }).catch(err => {
-//     console.log(err);
-// });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
 const app = require('./app');
+
+// Connect to database
+const DB_URI = process.env.DATABASE_URI;
+mongoose.connect(DB_URI, {
+    useUnifiedTopology:true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log('Database connection successfull'));
 
 // Start server
 const port = process.env.PORT || 8080;
