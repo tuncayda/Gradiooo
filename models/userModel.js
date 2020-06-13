5da9dd6487561ff3c7aba9cc309f983c4ea3dc4e
Tuncay Dagdelen <tuncaydagdelen89@users.noreply.github.com>
2020-06-13 18:19:05 +0200

    Password management

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -22,8 +23,26 @@ const userSchema = new mongoose.Schema({
     },
     passwordConfirm: {
         type: String,
-        required: [true, 'Confirm password']
+        required: [true, 'Confirm password'],
+        validate: {
+            // Validate only works on create and save
+            validator: function(el) {
+                return el === this.password;
+            },
+            message: 'Passwords are not the same'
+        }
     }
+});
+
+// Pre hook runs between getting the data and pushing it to the database
+userSchema.pre('save', async function(next) {
+    // Only run this function if password was modified
+    if (!this.isModified('password')) return next();
+
+    // Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+    this.passwordConfirm = undefined;
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
