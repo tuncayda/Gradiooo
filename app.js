6ca3b26705f4ec30576327a5629a86496e303a88
Tuncay Dagdelen <tuncaydagdelen89@users.noreply.github.com>
2020-06-21 12:16:53 +0200

    Configured pug

--- app.js
+++ app.js
@@ -7,17 +7,25 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+// Set template engine
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // Middlewares for serving assets, logging responses and parsing json
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 app.use(express.json());
 
 // Route for serving html
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+app.get('/', (req, res) => {
+    res.status(200).render('base');
 });
+
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/index.html'));
+// });
 
 // Mounting routers to endpoints
 app.use('/api/v1/users', userRouter);
