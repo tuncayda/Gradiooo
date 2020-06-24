3782273beadd7d80d9f96bc6b0e35026bce07c4a
Tuncay Dagdelen <tuncaydagdelen89@users.noreply.github.com>
2020-06-24 22:27:17 +0200

    revert to  7f98609

---
+++ public/scripts/routes/colorRoutes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const colorController = require('./../controllers/colorController');
+const authController = require('./../controllers/authController');
+
+// Router obj
+const router = express.Router();
+
+// Router with parameter
+router.param('id', (req, res, next, val) => {
+    console.log(`Tour id is ${val}`);
+    next();
+});
+
+router
+    .route('/')
+    .get(colorController.getAllColors)
+    // .get(authController.protect, colorController.getAllColors)
+    .post(colorController.createColor);
+
+router
+    .route('/:id')
+    .get(colorController.getColor)
+    .patch(colorController.updateColor)
+    .delete(
+        authController.protect,
+        authController.restrictTo('admin'), 
+        colorController.deleteColor);
+
+module.exports = router;
