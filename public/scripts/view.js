3ca8bf71bdc0fc0e1613e8eef16b91683fa65c92
Tuncay Dagdelen <10659194+tuncayda@users.noreply.github.com>
2020-08-28 00:42:49 +0200

    Added ability to select row or column view

---
+++ public/scripts/view.js
@@ -0,0 +1,9 @@
+function columnView() {
+    let main = document.getElementById('main__content');
+    main.classList.add('column');
+}
+
+function rowView() {
+    let main = document.getElementById('main__content');
+    main.classList.remove('column');
+}
