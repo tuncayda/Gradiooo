cc1defdb039a919193120c826282cc8ef8db9f49
Tuncay Dagdelen <10659194+tuncayda@users.noreply.github.com>
2020-08-28 10:55:16 +0200

    Refactored

--- public/scripts/view.js
+++ public/scripts/view.js
@@ -1,9 +1,16 @@
+// Remember if user had column or row view in the previous session
+if (localStorage.getItem('view') === 'column') {
+    columnView();
+}
+
 function columnView() {
     let main = document.getElementById('main__content');
     main.classList.add('column');
+    localStorage.setItem('view', 'column');
 }
 
 function rowView() {
     let main = document.getElementById('main__content');
     main.classList.remove('column');
+    localStorage.removeItem('view');
 }
