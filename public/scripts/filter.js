81bc7ec598a72c5bf7bcf836e037e5726867b8d7
Tuncay Dagdelen <tuncaydagdelen89@users.noreply.github.com>
2020-09-06 08:28:34 +0200

    Altered style for the dropdown color when filtering

--- public/scripts/filter.js
+++ public/scripts/filter.js
@@ -10,9 +10,9 @@ let filtered = new Array();
 function filterColor(color, e) {
     if (!filtered.includes(color.target.id)) {
         filtered.push(color.target.id);
-        e.style = 'border: 2px solid black';
+        e.classList.add('dropdown__circle--selected')
     } else {
-        e.style = 'border: none';
+        e.classList.remove('dropdown__circle--selected')
         filtered.splice(filtered.indexOf(color.target.id), 1);
     }
     let mainContent = document.getElementById('main__content').childNodes;
